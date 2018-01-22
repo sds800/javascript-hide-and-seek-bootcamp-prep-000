ca7168dacebb4eef2ece1e5824c0698d165ed44a
SDS 800 <jeremyhyman@gmail.com>
2018-01-22 22:43:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 function deepestChild(){
   depth = document.getElementById('grand-node').querySelectorAll('div')
 for (let i = 0; i < depth.length; i++){
+  depth[i].innerHTML =
+  
   
 } 
     
