475d990133baa0ce31e4be771b13b4cf93bb9f1c
SDS 800 <jeremyhyman@gmail.com>
2018-01-22 22:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
 function deepestChild(){
   depth = document.getElementById('grand-node').querySelectorAll('div')
-  while(depth )
+for (let i = 0; i < depth.length; i++){
+  
+} 
     
   
   
