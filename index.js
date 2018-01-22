1bcb72c78e1ed9c356bdc87439bc1084e77ea054
SDS 800 <jeremyhyman@gmail.com>
2018-01-22 19:10:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector){
+  
+  var first = document.querySelector(selector);
+  return first;
+  
+}
+
+function nestedTarget(){
+  
+  return document.getElementById('nested');
+  
+}
+
+function increaseRankBy(n){
+  
+const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + [1] + n).toString()
+} 
+  
+}
+
+function deepestChild(){
+  
+  
+  
+}
