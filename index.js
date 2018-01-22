e7d9e0bb7059d5fe230245263e0b91ef5cc4952e
SDS 800 <jeremyhyman@gmail.com>
2018-01-22 21:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n){
   
 const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
  for (let i = 0; i < lis.length; i++) {
-  parseInt(lis[i].innerHTML) = (i + [n]).toString()
+  parseInt(lis[i].innerHTML)
 } 
   
 }
